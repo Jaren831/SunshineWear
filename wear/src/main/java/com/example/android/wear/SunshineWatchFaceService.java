d0c68f9f9e5b40fa83bafa892192070b8369d4c4
Jaren831 <jaren.lynchmobb@gmail.com>
2017-02-11 21:09:38 -0800

    More baseline

--- wear/src/main/java/com/example/android/wear/SunshineWatchFaceService.java
+++ wear/src/main/java/com/example/android/wear/SunshineWatchFaceService.java
@@ -16,14 +16,26 @@
 
 package com.example.android.wear;
 
-import android.graphics.Bitmap;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.icu.util.Calendar;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.text.format.Time;
 import android.view.SurfaceHolder;
+import android.view.WindowInsets;
+
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Digital watch face with seconds. In ambient mode, the seconds aren't displayed. On devices with
@@ -39,53 +51,210 @@ public class SunshineWatchFaceService extends CanvasWatchFaceService {
 
     /* implement service callback methods */
     private class Engine extends CanvasWatchFaceService.Engine {
-        static final int MSG_UPDATE_TIME = 0;
-        Calendar mCalendar;
+        //Member variables
+        private Typeface WATCH_TEXT_TYPEFACE = Typeface.create( Typeface.SERIF, Typeface.NORMAL );
 
-        //device features
-        boolean mLowBitAmbient;
+        private static final int MSG_UPDATE_TIME_ID = 42;
+        private long mUpdateRateMs = 1000;
 
-        //graphic objects
-        Bitmap mBackgroundBitmap;
-        Bitmap mBackgroundScaledBitmap;
-        Paint mHourPaint;
-        Paint mMinutePaint;
+        private Time mDisplayTime;
 
+        private Paint mBackgroundColorPaint;
+        private Paint mTextColorPaint;
 
+        private boolean mHasTimeZoneReceiverBeenRegistered = false;
+        private boolean mIsInMuteMode;
+        private boolean mIsLowBitAmbient;
+
+        private float mXOffset;
+        private float mYOffset;
+
+        private int mBackgroundColor = Color.parseColor( "black" );
+        private int mTextColor = Color.parseColor( "red" );
+
+        final BroadcastReceiver mTimeZoneBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mDisplayTime.clear( intent.getStringExtra( "time-zone" ) );
+                mDisplayTime.setToNow();
+            }
+        };
+
+        private final Handler mTimeHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch( msg.what ) {
+                    case MSG_UPDATE_TIME_ID: {
+                        invalidate();
+                        if( isVisible() && !isInAmbientMode() ) {
+                            long currentTimeMillis = System.currentTimeMillis();
+                            long delay = mUpdateRateMs - ( currentTimeMillis % mUpdateRateMs );
+                            mTimeHandler.sendEmptyMessageDelayed( MSG_UPDATE_TIME_ID, delay );
+                        }
+                        break;
+                    }
+                }
+            }
+        };
 
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
             /* initialize your watch face */
+            setWatchFaceStyle( new WatchFaceStyle.Builder( SunshineWatchFaceService.this )
+                    .setBackgroundVisibility( WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE )
+                    .setCardPeekMode( WatchFaceStyle.PEEK_MODE_VARIABLE )
+                    .setShowSystemUiTime( false )
+                    .build()
+            );
+
+            mDisplayTime = new Time();
+
+            initBackground();
+            initDisplayText();
+        }
+        private void initBackground() {
+            mBackgroundColorPaint = new Paint();
+            mBackgroundColorPaint.setColor( mBackgroundColor );
+        }
+
+        private void initDisplayText() {
+            mTextColorPaint = new Paint();
+            mTextColorPaint.setColor( mTextColor );
+            mTextColorPaint.setTypeface( WATCH_TEXT_TYPEFACE );
+            mTextColorPaint.setAntiAlias( true );
+            mTextColorPaint.setTextSize( getResources().getDimension( R.dimen.digital_text_size ) );
         }
 
         @Override
         public void onPropertiesChanged(Bundle properties) {
             super.onPropertiesChanged(properties);
             /* get device features (burn-in, low-bit ambient) */
+            if( properties.getBoolean( PROPERTY_BURN_IN_PROTECTION, false ) ) {
+                mIsLowBitAmbient = properties.getBoolean( PROPERTY_LOW_BIT_AMBIENT, false );
+            }
         }
 
         @Override
         public void onTimeTick() {
             super.onTimeTick();
             /* the time changed */
+            invalidate();
+
         }
 
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
             /* the wearable switched between modes */
+            if( inAmbientMode ) {
+                mTextColorPaint.setColor( Color.parseColor( "white" ) );
+            } else {
+                mTextColorPaint.setColor( Color.parseColor( "red" ) );
+            }
+
+            if( mIsLowBitAmbient ) {
+                mTextColorPaint.setAntiAlias( !inAmbientMode );
+            }
+
+            invalidate();
+            updateTimer();
         }
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
             /* draw your watch face */
+
+            mDisplayTime.setToNow();
+
+            drawBackground( canvas, bounds );
+            drawTimeText( canvas );
+        }
+        private void drawTimeText( Canvas canvas ) {
+            String timeText = getHourString() + ":" + String.format( "%02d", mDisplayTime.minute );
+            if( isInAmbientMode() || mIsInMuteMode ) {
+                timeText += ( mDisplayTime.hour < 12 ) ? "AM" : "PM";
+            } else {
+                timeText += String.format( ":%02d", mDisplayTime.second);
+            }
+            canvas.drawText( timeText, mXOffset, mYOffset, mTextColorPaint );
+        }
+
+        private String getHourString() {
+            if( mDisplayTime.hour % 12 == 0 )
+                return "12";
+            else if( mDisplayTime.hour <= 12 )
+                return String.valueOf( mDisplayTime.hour );
+            else
+                return String.valueOf( mDisplayTime.hour - 12 );
+        }
+
+        private void drawBackground( Canvas canvas, Rect bounds ) {
+            canvas.drawRect( 0, 0, bounds.width(), bounds.height(), mBackgroundColorPaint );
         }
 
         @Override
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
             /* the watch face became visible or invisible */
+            if( visible ) {
+                if( !mHasTimeZoneReceiverBeenRegistered ) {
+
+                    IntentFilter filter = new IntentFilter( Intent.ACTION_TIMEZONE_CHANGED );
+                    SunshineWatchFaceService.this.registerReceiver( mTimeZoneBroadcastReceiver, filter );
+
+                    mHasTimeZoneReceiverBeenRegistered = true;
+                }
+
+                mDisplayTime.clear( TimeZone.getDefault().getID() );
+                mDisplayTime.setToNow();
+            } else {
+                if( mHasTimeZoneReceiverBeenRegistered ) {
+                    SunshineWatchFaceService.this.unregisterReceiver( mTimeZoneBroadcastReceiver );
+                    mHasTimeZoneReceiverBeenRegistered = false;
+                }
+            }
+
+            updateTimer();
+        }
+
+        private void updateTimer() {
+            mTimeHandler.removeMessages( MSG_UPDATE_TIME_ID );
+            if( isVisible() && !isInAmbientMode() ) {
+                mTimeHandler.sendEmptyMessage( MSG_UPDATE_TIME_ID );
+            }
+        }
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets) {
+            super.onApplyWindowInsets(insets);
+
+            mYOffset = getResources().getDimension( R.dimen.digital_y_offset );
+
+            if( insets.isRound() ) {
+                mXOffset = getResources().getDimension( R.dimen.digital_x_offset_round );
+            } else {
+                mXOffset = getResources().getDimension( R.dimen.digital_x_offset );
+            }
+        }
+
+        @Override
+        public void onInterruptionFilterChanged(int interruptionFilter) {
+            super.onInterruptionFilterChanged(interruptionFilter);
+
+            boolean isDeviceMuted = ( interruptionFilter == android.support.wearable.watchface.WatchFaceService.INTERRUPTION_FILTER_NONE );
+            if( isDeviceMuted ) {
+                mUpdateRateMs = TimeUnit.MINUTES.toMillis( 1 );
+            } else {
+            }
+
+            if( mIsInMuteMode != isDeviceMuted ) {
+                mIsInMuteMode = isDeviceMuted;
+                int alpha = ( isDeviceMuted ) ? 100 : 255;
+                mTextColorPaint.setAlpha( alpha );
+                invalidate();
+                updateTimer();
+            }
         }
     }
 }
